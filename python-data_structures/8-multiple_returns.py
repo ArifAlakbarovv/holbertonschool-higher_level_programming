0e798447dd06c870bd4077d1438ee62757a87bf3
Arif <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-05-24 15:56:55 +0400

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) > 0:
         a = len(sentence)
         b = sentence[0]
-        tuple = (a, b)
-        return tuple
-    else:
-        return None
+        return (a, b)
+    return (0, None)
