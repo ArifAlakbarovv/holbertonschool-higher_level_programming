ebef22d2591c86e6b7162cdd3d19f643af892ea6
Arif <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-05-24 14:31:10 +0400

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             if j != i[len(i) - 1]:
                 print("{:d}".format(j, end = " ")
-            print("{:d}".format(j)
+            print("{:d}".format(j))
