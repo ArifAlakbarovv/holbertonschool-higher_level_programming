4f59d456ebb88b47621b18a1c024dc926c979168
Arif <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-05-25 08:47:40 +0400

    Update 3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,6 +2,6 @@
 def common_elements(set_1, set_2):
     new_set = []
     for i in range(len(set_1)):
-        if i in set_2:
+        if set_1[i] in set_2:
             new_set.append(set_1[i])
     return new_set
