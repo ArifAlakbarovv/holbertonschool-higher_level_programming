482a1558d88c28fe394c56242e57b3723b0d608b
Arif <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-05-25 08:35:42 +0400

    Create 2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    a = 0
+    for i in range(len(my_list):
+        if my_list[i] not new_list:
+            a += my_list[i]
+            new_list.append(my_list[i])
+    return a
